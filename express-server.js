bf21faf19fe07bd16a12fc4af849097221329287
Andrew <abarrett879@gmail.com>
2021-03-15 14:48:14 -0500

    Initial commit
    Working Express server receives requests to open in Chrome.

---
+++ express-server.js
@@ -0,0 +1,60 @@
+/* Author: Andrew Barrett
+ * Date Created: 2/24/21
+ * Purpose: Set up a web server that listens for POST requests on the local network to display a web page.
+ *
+*/
+
+// Log to Event Viewer when being used as a Service
+var EventLogger = require('node-windows').EventLogger;
+var log = new EventLogger('Win-Handover');
+
+// Use Express to host the web server
+const express = require('express');
+const app = express()
+
+// Use 'child_process' to execute Powershell requests
+const { exec } = require("child_process");
+
+// Port to listen on    
+const port = 3000
+
+// "Password" is just a required json parameter in the POST request to make sure it comes from a device I've authorized
+const password = 'apple1857'
+
+// Install middleware to make json readable
+app.use(express.json())
+
+// Capture POST request
+app.post('/', (req, res) => {
+  log.info('Received url: ' + req.body.url);
+  
+  // Check that the request includes the password
+  if (req.body.password == password) {
+    // Create Powershell script to open Chrome from requested url
+    script = 'start chrome.exe "' + req.body.url + '"'
+    // Run the script
+    exec(script, (error, stdout, stderr) => {
+      // Log errors
+      if (error) {
+          log.error(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          log.error(`stderr: ${stderr}`);
+          return;
+      }
+    });
+    res.send("Works");
+  }
+})
+
+// Listen for requests on a given port
+app.listen(port, () => {
+  log.info(`WindowsHandoff listening at http://localhost:${port}`)
+})
+
+// we want to run 'start chrome www.macrumors.com' , with the url provided in req
+
+// idea is to use Express to listen for a POST request containing the desired url
+
+// then we will run a batch script that opens the webpage
---
+++ runner.js
@@ -0,0 +1,26 @@
+/* Author: Andrew Barrett
+ * Date Created: 2/24/21
+ * Purpose: Set up a web server that listens for POST requests on the local network to display a web page.
+ *
+*/
+
+// Use node-windows to run this script in the background as a service (and support launch on login)
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'Windows Handoff',
+  description: 'Allows opening websites in Chrome that are open in Safari on an iPhone',
+  script: require('path').join(__dirname,'express-server.js')
+  //, workingDirectory: '...'
+  //, allowServiceLogon: true
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+  console.log('installed');
+});
+
+svc.install();
